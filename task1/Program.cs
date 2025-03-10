9e294efe68b182421b34da9090a2fa54d0040467
Lenovo <nazrinna-bb208@code.edu.az>
2025-03-11 01:05:50 +0400

    Proje dosyası ekle.

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//task1->4
+
+
+for (int i = 0; i < 100; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+
+}
+int a = 0;
+while (a < 100)
+{
+    if (a % 2 == 0)
+    {
+        Console.WriteLine(a);
+
+    }
+    a++;
+}
